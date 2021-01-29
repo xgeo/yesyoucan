4782a397e6d30e3bd2c0084567e35e4e666d0201
Geovanny Lino Coutinho <geovannylc@gmail.com>
2021-01-28 21:50:50 -0300

    #TwoIndexValues

--- scripts/two-sum.js
+++ scripts/two-sum.js
@@ -1,16 +1,7 @@
 /**
  * Two Sum
  *
- * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- *
- * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- *
- * Example:
- *
- * Given nums = [2, 7, 11, 15], target = 9,
- *
- * Because nums[0] + nums[1] = 2 + 7 = 9,
- * return [0, 1].
+ * Sum two values - index 0 and index 1
  */
 
 var assert = require('assert');
